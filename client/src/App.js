01437df35c8129d9e391a147ef22f34c77a40ff4
ARTHUR EDUARDO O. ALVES DA SILVA <aoliveira@labcetec.net>
2024-06-07 21:30:55 -0300

    attua

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Cliente from './views/cliente/cliente';
-import Loja from './views/loja/loja';
-import Sidebar from './components/sidebar';
-import Logar from './views/Logar/index';
-import Nota from './views/nota/nota'
-import Agendamentos from './views/agendamento/agendamento'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import RotasSide from './RotasSide';
 
 function App() {
   return (
+
     <BrowserRouter>
-      <Sidebar/>
       <Routes>
-      <Route path='/' element={<Logar/>}/>
-        <Route path='/cliente' element={<Cliente/>}/>
-        <Route path='/loja' element={<Loja/>}/>
-        <Route path='/nota' element={<Nota/>}/>
-        <Route path='/agendamento' element={<Agendamentos/>}/>
+        <Route path="/*" element={<RotasSide />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
